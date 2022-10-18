522e2c8714c332e195cc22510ebb6bb9178f06df
scw3812 <scw3812@naver.com>
2022-10-18 15:54:52 +0900

    tailwind, eslint, prettier setting

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:@typescript-eslint/recommended', 'next', 'next/core-web-vitals', 'prettier'],
+};
