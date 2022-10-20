ed5acbbb1bfa9bd5315e4bfaa56eb5ab81ec1c91
scw3812 <scw3812@naver.com>
2022-10-20 15:59:25 +0900

    tailwind config

---
+++ plugins/scrollbar-hide.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const plugin = require('tailwindcss/plugin');
+
+const scrollbarHide = plugin(function ({ addUtilities }) {
+  addUtilities({
+    // . + 유틸리티 이름
+    '.scrollbar-hide': {
+      'scrollbar-width': 'none',
+      '-ms-overflow-style': 'none',
+      '&::-webkit-scrollbar': {
+        display: 'none',
+      },
+    },
+  });
+});
+
+module.exports = scrollbarHide;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
+const pxToRem = (px, base = 16) => `${px / base}rem`;
+const pxrConfig = [...Array(100).keys()]
+  .map((key) => key + 1)
+  .reduce((acc, px) => {
+    acc[`${px}pxr`] = pxToRem(px);
+    return acc;
+  }, {});
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      mobile: '360px', // @media (min-width: 360px)
+      foldable: '523px', // @media (min-width: 523px)
+      tablet: '768px', // @media (min-width: 768px)
+      web: '976px', // @media (min-width: 976px)
+      wide: '1440px', // @media (min-width: 1440px)
+      'under-mobile': { max: '359px' }, // @media (max-width: 359px)
+      'under-foldable': { max: '522px' }, // @media (max-width: 522px)
+      'under-tablet': { max: '767px' }, // @media (max-width: 767px)
+    },
+    extend: {
+      spacing: {
+        ...pxrConfig,
+      },
+    },
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [require('./plugins/scrollbar-hide')],
 };
